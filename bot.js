280ece0918592f993a207cfa4c3ae579f3e313be
SplashHazard <dillonbayless1@gmail.com>
2017-01-08 12:35:12 -0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,18 +4,30 @@ const auth = require('./settings.js');
 
 bot.on('message', (message) => {
     if (message.author.id !== bot.user.id) return;
-    if (message.content.startsWith(auth.prefix)) return;
-    let commmand = message.content.slice(auth.prefix.length);
+    if (!message.content.startsWith(auth.prefix)) return;
+    let command = message.content.slice(auth.prefix.length);
     let split = command.split(" ");
     command = split[0];
     split.shift();
     let code = split.join(" ");
-    if (message.content.startsWith('eval')) {
+    if (command === "eval") {
         try {
-        let ev = eval(code);
-        
+        let ev = require('util').inspect(eval(code));
+        if (ev.length > 1950) {
+            ev = ev.substr(0, 1950);
+        }
+        let token = auth.token.replace(/\./g, "\.")
+        let re = new RegExp(token, 'g') 
+        ev = ev.replace(re, "*R-eD-Ac-Te-D-*");
+        message.channel.sendMessage("**Input:**```js\n"+code+"```**Eval:**```js\n"+ev+"```")
         } catch(err) {
             message.channel.sendMessage('```js\n'+err+"```")
         }
     }
 })
+
+bot.on('ready', () => {
+    console.log("Ready To Eval")
+})
+
+bot.login(auth.token)
